9886746bbf1b42b4c306c47e66a67f48dbb8744c
Magistra <magistraagis@gmail.com>
2023-01-25 00:06:11 +0800

    setting fetch

--- fetch.js
+++ fetch.js
@@ -1,3 +1,11 @@
-$(selector).on(click, function () {
-   
+$('#search-btn').on('click', function () {
+    $.ajax({
+        type: "get",
+        url: "http://api.alquran.cloud",
+        
+        dataType: "json",
+        success: function (response) {
+            console.log(response.data);
+        }
+    });  
 });
