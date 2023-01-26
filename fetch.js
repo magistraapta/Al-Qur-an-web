285d6b9bc954ae288c6ee5988e7863b6ddb26a09
Magistra <magistraagis@gmail.com>
2023-01-26 22:20:40 +0800

    change api

--- fetch.js
+++ fetch.js
@@ -1,35 +1,54 @@
 $.ajax({
   type: 'get',
-  url: 'http://api.alquran.cloud/v1/surah',
+  url: 'https://api.quran.com/api/v4/chapters?language=en',
   dataType: 'json',
   success: function (res) {
-    if (res.code == 200) {
-      let surah = res.data;
-      $.each(surah, function (i, data) {
-        $('#surah-list').append(`
+    let surah = res.chapters;
+    console.log(surah);
+    $.each(surah, function (i, data) {
+      $('#surah-list').append(
+        `
             <div id="surah-list" class="container col-md-7 mt-2">
-                <div class="col text-success">
-                    <div class=" d-flex justify-content-between bg-success-subtle rounded-1 px-2 py-2 ">
+                <div class="col text-success text-decoration-none">
+                    <a id="surah-click" class=" d-flex justify-content-between bg-primary-subtle rounded-1 px-2 py-2 text-decoration-none ">
                         <div class=" ">
-                            <h5 id="surah-title" class="m-0">`+data.englishName+`</h5>
-                                <p id="total-ayat" class="m-0">`+ data.numberOfAyahs+`</p>
+                            <h5 id="surah-title" class="m-0"> ${data.name_simple} </h5>
+                                <p id="total-ayat" class="m-0">${data.verses_count}</p>
                         </div>
-                        <h4 id="title-arabic" class="m-0">`+ data.name +`</h4>
-                    </div>
+                        <h4 id="title-arabic" class="m-0">${data.name_arabic}</h4>
+                    </a>
                 </div>
             </div>
-      `);
-    });
-    } else {
-      $('#surah-title').html(
-        `
-        <div class="col">
-        <h1 class=" text-center">
-          `+ res.Error+`
-          </h1>
-        </div>
-        `
+      `
       );
-    }
+    });
   },
 });
+
+// $('#search-btn').on('click', function () {
+//   let surah = $('#search-input').val();
+//   $.ajax({
+//     type: 'get',
+//     url: `http://api.alquran.cloud/v1/surah/${surah}`,
+//     dataType: 'json',
+//     success: function (res) {
+//       let ayat = res.data.ayahs
+//       $.each(ayat, function (i, data) {
+//         $('#surah-list').append(
+//           `
+//               <div id="surah-list" class="container col-md-7 mt-2">
+//                   <div class="col text-success text-decoration-none">
+//                       <a id="surah-click" class=" d-flex justify-content-between bg-primary-subtle rounded-1 px-2 py-2 text-decoration-none ">
+//                           <div class=" ">
+//                               <h5 id="surah-title" class="m-0"> ${data.text} </h5>
+//                           </div>
+//                       </a>
+//                   </div>
+//               </div>
+//         `
+//         );
+
+//       });
+//     },
+//   });
+// });
